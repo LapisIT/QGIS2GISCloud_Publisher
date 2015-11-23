bc99cf26a0b11b9a874f73a75cac5563e4b27552
MKingspatial <Michael.King@live.com.au>
2015-11-23 13:29:33 +1100

    Adding the base processing plugin template

---
+++ GISCloudUpload/giscloud_uploader.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+/***************************************************************************
+ GISCloudUpload
+                                 A QGIS plugin
+ Uploader to GIs cloud
+                              -------------------
+        begin                : 2015-11-23
+        copyright            : (C) 2015 by Spatial Vision
+        email                : michael.king@spatialvision.com.au
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'Spatial Vision'
+__date__ = '2015-11-23'
+__copyright__ = '(C) 2015 by Spatial Vision'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+import os
+import sys
+import inspect
+
+from processing.core.Processing import Processing
+from giscloud_uploader_provider import GISCloudUploadProvider
+
+cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+
+class GISCloudUploadPlugin:
+
+    def __init__(self):
+        self.provider = GISCloudUploadProvider()
+
+    def initGui(self):
+        Processing.addProvider(self.provider)
+
+    def unload(self):
+        Processing.removeProvider(self.provider)
